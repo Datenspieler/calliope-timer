16f39de91a51569b6a843285672d175fe62502a6
Datenspieler <christopher@datenspieler.com>
2021-10-30 06:17:19 +0200

    wip2

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 def on_button_pressed_a():
     global t
+    basic.show_string("" + (statusTxtList[statusNr]))
     if statusNr == 0:
-        basic.show_string("" + (statusTxtList[statusNr]))
         t += 1 * 600
+        ZeigeTimer(t)
+    elif statusNr == 1:
+        t += 1 * 60
+        ZeigeTimer(t)
+    elif statusNr == 2:
+        t += 1 * 10
+        ZeigeTimer(t)
+    elif statusNr == 3:
+        t += 1 * 1
         ZeigeTimer(t)
     else:
         pass
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def ZeigeTimer(t: number):
-    basic.show_string(convert_to_text(t))
+def ZeigeTimer(t0: number):
+    basic.show_string(convert_to_text(t0))
 statusTxtList: List[str] = []
 statusNr = 0
 t = 0
@@ -22,6 +31,7 @@ statusTxtList = ["setze10Min",
     "setzeTon",
     "laufe",
     "abgelaufene"]
+ton = False
 basic.clear_screen()
 
 def on_forever():
